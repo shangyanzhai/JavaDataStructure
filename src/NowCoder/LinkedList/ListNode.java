f080b3069adf746605cc9cb54d27666df480c100
殇雁斋 <2091163852@qq.com>
2023-03-27 14:57:09 +0800

    3_27

---
+++ src/NowCoder/LinkedList/ListNode.java
@@ -0,0 +1,14 @@
+package NowCoder.LinkedList;
+
+public class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+    ListNode(int val , ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
---
+++ src/NowCoder/LinkedList/PalindromeList.java
@@ -0,0 +1,80 @@
+package NowCoder.LinkedList;
+
+/**
+ *    OR36 链表的回文结构
+ *
+ *
+ * 对于一个链表，请设计一个时间复杂度为O(n),额外空间复杂度为O(1)的算法，判断其是否为回文结构。
+ *
+ * 给定一个链表的头指针A，请返回一个bool值，代表其是否为回文结构。保证链表长度小于等于900。
+ *
+ * 测试样例：
+ * 1->2->2->1
+ * 返回：true
+ */
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class PalindromeList {
+    public int sizeof(ListNode head){
+        int num = 0;
+
+        ListNode cur = head;
+        while(cur != null){
+            cur = cur.next;
+            num++;
+        }
+
+        return num;
+    }
+
+    public boolean chkPalindrome(ListNode A) {
+        if(A == null){
+            return false;
+        }
+        // write code here
+        Deque<Integer> stack = new LinkedList<>();
+
+        int num = sizeof(A);
+
+        if(num == 1){
+            return true;
+        }
+
+        int a = num % 2;
+        num = num / 2;
+
+        for(int i = 0;i < num;i++){
+            stack.push(A.val);
+            A = A.next;
+        }
+
+        if(a == 0){//此时代表是偶数个长度
+            while(A != null){
+                if(A.val != stack.pop()){
+                    return false;
+                }
+                A = A.next;
+            }
+        }else{
+            A = A.next;
+            while(A != null){
+                if(A.val != stack.pop()){
+                    return false;
+                }
+                A = A.next;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        ListNode list1= new ListNode(1);
+        ListNode list2 = new ListNode(2,list1);
+//        ListNode list3 = new ListNode(2,list2);
+        ListNode list = new ListNode(3,list2);//{1,2,2,1}
+        PalindromeList palindromeList = new PalindromeList();
+        System.out.println(palindromeList.chkPalindrome(list));
+    }
+}
